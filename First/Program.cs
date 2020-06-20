f4e27a7091e8a7773c41bd125e6cf0a12915d0e7
amish99 <amishtangri123@gmail.com>
2020-06-19 23:20:58 -0400

    Add project files.

---
+++ First/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person("Tom", 40);
+
+            tom.sayshello();
+
+            string tomString = tom.ToString();
+
+            Console.WriteLine(tomString);
+            Console.WriteLine();
+
+
+            Person jacky = new Person("Jacky", 22);
+
+            jacky.sayshello();
+
+            Console.WriteLine(jacky.ToString());
+            Console.WriteLine();
+
+            /*Student joss = new Student("Joss", 22, "A1234567890");
+
+            joss.SaysHello();
+            joss.Studies();
+
+            Console.WriteLine(joss.ToString());
+
+
+            // wait for user to enter anything*/
+            Console.ReadLine();
+        }
+    }
+}
